670e61e42d4f683695432bdcc38294752237d865
pmutisya <pmutisya@meliora.tech>
2018-06-24 22:50:59 +0300

    Jhipster basic java app

---
+++ src/main/webapp/app/app.module.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('aminiApp', [
+            'ngStorage',
+            'ngResource',
+            'ngCookies',
+            'ngAria',
+            'ngCacheBuster',
+            'ngFileUpload',
+            'ui.bootstrap',
+            'ui.bootstrap.datetimepicker',
+            'ui.router',
+            'infinite-scroll',
+            // jhipster-needle-angularjs-add-module JHipster will add new module here
+            'angular-loading-bar',
+            'ngMap'
+        ])
+        .run(run);
+
+    run.$inject = ['stateHandler'];
+
+    function run(stateHandler) {
+        stateHandler.initialize();
+    }
+})();
